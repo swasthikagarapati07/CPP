51940abdb60ca3ce0798f842c321437d00bd57e7
swasthikagarapati07 <swasthikagarapati@gmail.com>
2026-07-20 13:04:30 +0530

    Add files via upload

---
+++ Namespace college using scope resolution operator(7).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+namespace College
+{
+	void displayCollege()
+	{
+		cout<<"Welcome to Aditya University!"<<endl;
+	}
+}
+int main()
+{
+	College::displayCollege();
+	return 0;
+}
