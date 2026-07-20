e73a87bbd44382d46b3a34f877f13d3a19ac4746
swasthikagarapati07 <swasthikagarapati@gmail.com>
2026-07-20 13:05:03 +0530

    Add files via upload

---
+++ Addition of two integers(9).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    sum = a + b;
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
