7c4b9471b893a4aa2cc5cf04614a3bfacc82af3f
swasthikagarapati07 <swasthikagarapati@gmail.com>
2026-09-17 10:24:35 +0530

    Add files via upload

---
+++ Inheritance(26).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void eat() {
+        cout << "Animal eats" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Dog d;
+    d.eat();
+    d.bark();
+    return 0;
+}
