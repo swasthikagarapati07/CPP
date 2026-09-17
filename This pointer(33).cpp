3a256f1664fa6109fdfa3ee47672b96b39904ab8
swasthikagarapati07 <swasthikagarapati@gmail.com>
2026-09-17 10:47:22 +0530

    Add files via upload

---
+++ This pointer(33).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    int age;
+public:
+    void setAge(int age) {
+        this->age = age;
+    }
+
+    void display() {
+        cout << "Age = " << this->age << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.setAge(20);
+    s.display();
+    return 0;
+}
+
