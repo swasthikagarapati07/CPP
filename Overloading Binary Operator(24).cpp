01cce1a6113f85ec90755d8be86baf2de87badcc
swasthikagarapati07 <swasthikagarapati@gmail.com>
2026-09-17 10:19:42 +0530

    Add files via upload

---
+++ Overloading Binary Operator(24).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+    float real, imag;
+public:
+    Complex(float r = 0, float i = 0) : real(r), imag(i) {}
+
+    Complex operator+(const Complex& c) {
+        return Complex(real + c.real, imag + c.imag);
+    }
+
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(2, 3), c2(4, 5);
+    Complex c3 = c1 + c2;
+    c3.display();
+    return 0;
+}
