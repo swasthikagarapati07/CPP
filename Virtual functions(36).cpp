f8cae107367a75e3c2d1b57898a4110f985fd215
swasthikagarapati07 <swasthikagarapati@gmail.com>
2026-09-17 10:51:35 +0530

    Add files via upload

---
+++ Virtual functions(36).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Animal* p = new Dog;
+    p->sound();
+    delete p;
+    return 0;
+}
+
